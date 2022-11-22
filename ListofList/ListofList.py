ed0fcab4feab36fc46ab1246f607f3dac06ea130
Nashwan Adenaya <75899276+OneRicez@users.noreply.github.com>
2022-11-22 22:49:09 +0700

    Update ListofList.py

--- ListofList/ListofList.py
+++ ListofList/ListofList.py
@@ -74,18 +74,21 @@ def make_set(L):
     else:
       return konso(first_element(L), make_set(tail(L)))
 
-# def isEqs(S1,S2):
-#   if is_empty_LoL(S1) and is_empty_LoL(S2): True
-#   elif not(is_empty_LoL(S1)) and is_empty_LoL(S2): False
-#   elif is_empty_LoL(S1) and not(is_empty_LoL(S2)): False
-#   elif not(is_empty_LoL(S1)) and not(is_empty_LoL(S2)): 
-#     if is_atom(first_List(S1)) and is_atom(last_List(S2)):
-#       first_List(S1) = first_List(S2) and isEqs(tail_List(S1),tail_List(S2))
+def isEqs(S1,S2):
+  if is_empty_LoL(S1) and is_empty_LoL(S2): True
+  elif not(is_empty_LoL(S1)) and is_empty_LoL(S2): False
+  elif is_empty_LoL(S1) and not(is_empty_LoL(S2)): False
+  elif not(is_empty_LoL(S1)) and not(is_empty_LoL(S2)): 
+    if is_atom(first_list(S1)) and is_atom(last_list(S2)):
+      return first_list(S1) == first_list(S2) and isEqs(tail_list(S1),tail_list(S2))
+    if isList(first_list(S1)) and isList(first_list(S2)):
+      return isEqs(first_list(S1),first_list(S2)) and isEqs(tail_list(S1),tail_list(S2))
+  else:
+    return False 
     
 def IsOneELmt(L):
   return len(L) == 1
 
-# print(max(L))
 def max2(a, b):
   if a >= b:
     return a
@@ -98,3 +101,17 @@ def maxL(L):
   else:
     max2(last_List(L),max(head_List(L)))
 
+def rember(a,L):
+  if is_empty_LoL(L):
+    return L
+  else:
+    if is_list(first_list(L)):
+      print(L, "1")
+      return konso_LoL(rember(a,first_list(L)),rember(a,tail_list(L)))
+    else:
+      if first_element(L) == a:
+        print(L, "2") 
+        return rember(a,tail_list(L))
+      else: 
+        print(L, "3")
+        return konso_LoL(first_element(L),rember(a,tail_list(L)))
